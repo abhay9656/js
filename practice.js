b3869356058b43048d6c25373b100d4ce0d41d1d
abhay9656 <abhay9656@gmail.com>
2024-04-09 20:11:03 +0530

    create object of mobile phone

--- practice.js
+++ practice.js
@@ -9,7 +9,28 @@
 // arr.splice(2,0,'f')
 // console.log(arr);
 
-const str=[2,3,4,1,5,6,9,10]
+// const str=[2,3,4,1,5,6,9,10]
 
-const double=str.filter(a=>a%2==0).map((b)=>{ return b**3 })
-console.log(double);
+// const double=str.filter(a=>a%3==0).map((b)=>{ return b**3 })
+// console.log(double);
+
+const obj=[
+    {name:"samsung",
+    color:['black',  'olive green','white'],
+    model:'s24'
+   },
+   {name:"oneplus",
+   color:['black','green','white'],
+   model:'11pro'
+  },
+  {name:"apple",
+  color:['black','green','white'],
+  model:'iphone 15'
+ },
+]
+
+const fil=obj.filter((a)=>{
+    return a.color.includes('black')
+}).map((a)=>{return a.name})
+
+console.log(fil);
