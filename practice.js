6a383e10a1a0d683f0cc9e58228838b3f9e0d549
abhay9656 <abhay9656@gmail.com>
2024-04-09 19:39:05 +0530

    use splice and shift method

--- practice.js
+++ practice.js
@@ -1,3 +1,10 @@
 let  arr=['a','b','c','d']
-arr.push(2)
+// arr.push(2)
+// console.log(arr);
+
+// console.log(arr.slice(-3,-1));
+arr.shift()
+console.log(arr);
+
+arr.splice(2,0,'f')
 console.log(arr);
