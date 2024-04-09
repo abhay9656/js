903db7febb87c110abcb73ce2438faa3f99656eb
abhay9656 <abhay9656@gmail.com>
2024-04-09 20:16:11 +0530

    uncomment all the previous code

--- practice.js
+++ practice.js
@@ -1,18 +1,18 @@
-// let  arr=['a','b','c','d']
-// // arr.push(2)
-// // console.log(arr);
-
-// // console.log(arr.slice(-3,-1));
-// arr.shift()
+let  arr=['a','b','c','d']
+// arr.push(2)
 // console.log(arr);
 
-// arr.splice(2,0,'f')
-// console.log(arr);
+// console.log(arr.slice(-3,-1));
+arr.shift()
+console.log(arr);
 
-// const str=[2,3,4,1,5,6,9,10]
+arr.splice(2,0,'f')
+console.log(arr);
 
-// const double=str.filter(a=>a%3==0).map((b)=>{ return b**3 })
-// console.log(double);
+const str=[2,3,4,1,5,6,9,10]
+
+const double=str.filter(a=>a%3==0).map((b)=>{ return b**3 })
+console.log(double);
 
 const obj=[
     {name:"samsung",
