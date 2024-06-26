8a432f7eb842d067925613639705cd2b414442ec
abhay9656 <abhay9656gupta@gmail.com>
2024-06-26 17:00:28 +0530

    Refactor code to capitalize the first letter of each word in a string

--- integer.js
+++ integer.js
@@ -5,12 +5,18 @@
 
 // console.log(isInteger(45));
 
-const arr=[1,2,3,4,4,5,5]
-let c=0
-const rep=arr.filter((item,index)=>{
-     return arr.indexOf(item)=== index
-})
-console.log(rep);
+// const arr=[1,2,3,4,4,5,5]
+// let c=0
+// const rep=arr.filter((item,index)=>{
+//      return arr.indexOf(item)=== index
+// })
+// console.log(rep);
 
-const set=[...new Set(arr)]
-console.log(set);
+// const set=[...new Set(arr)]
+// console.log(set);
+
+let str="hello js"
+const cap=str.split('').map((item)=>{
+    return item.charAt(0).toUpperCase()+item.slice(1)
+})
+console.log(cap.join(''));
