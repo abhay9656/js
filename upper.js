8a0cff65cb4314296f500a0d20e194fddae4c460
abhay9656 <abhay9656gupta@gmail.com>
2024-06-27 20:00:33 +0530

    Refactor code to capitalize the first letter of each word in a string and optimize array sum calculation

--- upper.js
+++ upper.js
@@ -1,9 +1,16 @@
 
-function str(st){  
-   return st.split(" ").map((item)=>{
-       return item.charAt(0).toUpperCase()+item.slice(1)
-    }).join(' ')
+// function str(st){  
+//    return st.split(" ").map((item)=>{
+//        return item.charAt(0).toUpperCase()+item.slice(1)
+//     }).join(' ')
+// }
+
+// console.log(str("hello js"));
+
+const arr=[1,2,3,4,5]
+
+function sum()
+{
+    return arr.filter((item)=> item % 2!=0).reduce((acc,num)=> acc+num**2)
 }
-
-console.log(str("hello js"));
-
+console.log(sum());
